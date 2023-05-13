539484a3e90e50643d37d50cc3249e0f2166eb5f
Gaurav Khochare <khocharegaurav85@gmail.com>
2023-05-13 21:10:47 +0530

    Addition of new controller with hard codd values

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,51 @@
+﻿using DemoAPI.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        public RegionsController() { }
+
+        [HttpGet]
+
+        public IActionResult GetAll() 
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    RegionName="Auckland",
+                    RegionCode="AUK",
+                    RegionImageURL=""
+           
+                },
+                new Region
+                {
+                    Id=Guid.NewGuid(),
+                    RegionName="Wellington",
+                    RegionCode="WLT",
+                    RegionImageURL=""
+
+                },
+                new Region
+                {
+                    Id=new Guid(),
+                    RegionName="Auckland",
+                    RegionCode="AUK",
+                    RegionImageURL=""
+
+                }
+                
+
+            };
+            return Ok(regions);
+        }
+
+        
+    }
+}
